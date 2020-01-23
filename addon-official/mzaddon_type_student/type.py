e9d478fa3cb47b32178f5cb85ef27e47a356c87b
guzman-raphael <raphael.h.guzman@gmail.com>
2020-01-23 07:01:34 -0600

    Add change example.

--- addon-official/mzaddon_type_student/type.py
+++ addon-official/mzaddon_type_student/type.py
@@ -3,7 +3,7 @@ class Student:
     def __init__(self, first_name=None, last_name=None, enrolled=None):
         self.first_name = first_name
         self.last_name = last_name
-        self.enrolled = enrolled
+        self.enrolled = enrolled2
 
     def __str__(self):
         return str(self.__dict__)
